bbb50e93521d446a20c2a42cba17e4665940aa4f
anthonylxd <lxdblues@gmail.com>
2017-05-28 15:16:46 +0800

    秒杀系统（1）
    完成架构

---
+++ SSMStu/src/main/java/com/dto/SeckillExecution.java
@@ -0,0 +1,68 @@
+package com.dto;
+/*
+ * 封装秒杀执行后结果
+ */
+
+import com.entity.SuccessKilled;
+
+public class SeckillExecution {
+
+	private long seckillId;
+	
+	//秒杀执行结果状态
+	private int state;
+	//状态表示
+	private String stateInfo;
+	
+	//秒杀成功对象
+	private SuccessKilled successKilled;
+
+	public long getSeckillId() {
+		return seckillId;
+	}
+
+	public void setSeckillId(long seckillId) {
+		this.seckillId = seckillId;
+	}
+
+	public int getState() {
+		return state;
+	}
+
+	public void setState(int state) {
+		this.state = state;
+	}
+
+	public String getStateInfo() {
+		return stateInfo;
+	}
+
+	public void setStateInfo(String stateInfo) {
+		this.stateInfo = stateInfo;
+	}
+
+	public SuccessKilled getSuccessKilled() {
+		return successKilled;
+	}
+
+	public void setSuccessKilled(SuccessKilled successKilled) {
+		this.successKilled = successKilled;
+	}
+
+	public SeckillExecution(long seckillId, int state, String stateInfo, SuccessKilled successKilled) {
+		super();
+		this.seckillId = seckillId;
+		this.state = state;
+		this.stateInfo = stateInfo;
+		this.successKilled = successKilled;
+	}
+
+	public SeckillExecution(long seckillId, int state, String stateInfo) {
+		super();
+		this.seckillId = seckillId;
+		this.state = state;
+		this.stateInfo = stateInfo;
+	}
+	
+	
+}
